292622386efdf71cf652e0712ba66d38286600da
JamesPoppyCock <42442664+jamesly123@users.noreply.github.com>
2023-11-24 18:23:44 +0800

    UploadTest
    hehe

---
+++ Test.py
@@ -0,0 +1,14 @@
+package life.circles.website.test.runner;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import life.circles.website.test.testbase.TestBase;
+
+@RunWith(Cucumber.class)
